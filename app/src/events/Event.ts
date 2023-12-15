3c2d998f0aa0ef49c8bbe562e3ea7aa70070bd2c
KiraCorbett <kiracorbett00@gmail.com>
2023-12-14 18:49:06 -0800

    seperated EventList styles, fixed location bug

--- app/src/events/Event.ts
+++ app/src/events/Event.ts
@@ -13,7 +13,7 @@ export class Event {
         this.date = new Date(); // initialize with current date and time
         if (initializer.id) this.id = initializer.id;
         if (initializer.name) this.name = initializer.name;
-        if (initializer.location) this.location = initializer.description;
+        if (initializer.location) this.location = initializer.location;
         if (initializer.date) this.date = new Date(initializer.date) // if date provided it is used
       }
 }
